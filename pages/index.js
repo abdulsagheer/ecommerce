365da243d2956376081a1ba1591e1656780be6b1
abdulsagheer <abdulsagheeras29@gmail.com>
2022-06-09 16:22:47 +0530

    developing single product page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
-import React from 'react'
-import product from '../sanity_ecommerce/schemas/product';
-import { Product, FooterBanner, HeroBanner } from '../components/index'
+import React from "react";
 
-const Home = () => {
-  return (
-    <>
-      <HeroBanner />
-      <div className='products-heading'>
-        <h2>Beset Selling Products</h2>
-        <p>Speakers of many variations</p>
-        <div>{['Product1', 'Product2', 'Product3'].map((product) => product)}</div>
-      </div>
-      <FooterBanner />
-    </>
-  );
-}
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
-export default Home
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
+
+    <div className="products-container">
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
