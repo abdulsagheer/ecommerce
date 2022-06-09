ad0342cce470809602d1d0e768192ad7ca83b775
abdulsagheer <abdulsagheeras29@gmail.com>
2022-06-09 14:42:05 +0530

    used images from sanity

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
 
-const HeroBanner = () => {
+import React from "react";
+import Link from "next/link";
+
+import { urlFor } from "../lib/client";
+
+const HeroBanner = ({ heroBanner }) => {
   return (
-    <div>HeroBanner</div>
-  )
-}
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <h1>MID TEXT</h1>
+        <img src="" alt="headphones" className="hero-banner-image" />
 
-export default HeroBanner
+        <div>
+          <Link href="/product/id">
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import product from '../sanity_ecommerce/schemas/product';
+import { Product, FooterBanner, HeroBanner } from '../components/index'
 
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className='products-heading'>
         <h2>Beset Selling Products</h2>
         <p>Speakers of many variations</p>
         <div>{['Product1', 'Product2', 'Product3'].map((product) => product)}</div>
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
 }
